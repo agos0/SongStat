c75ec2f8d9e22145f004ce6f54af8cc866ed4502
agos0 <agoswami1238@gmail.com>
2025-09-03 21:48:43 -0400

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Script from "next/script";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
         <TempoInit />
+        <Analytics />
       </body>
     </html>
   );
